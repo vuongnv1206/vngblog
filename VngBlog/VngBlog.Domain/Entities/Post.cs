2a86e1b4a9a4149959513de1b4a97f32a149ed9f
NguyenVanVuong <hayasilver123@gmail.com>
2024-02-17 20:57:16 +0700

    Migration update post

--- VngBlog/VngBlog.Domain/Entities/Post.cs
+++ VngBlog/VngBlog.Domain/Entities/Post.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 using VngBlog.Domain.Abstractions;
 
@@ -11,16 +12,18 @@ namespace VngBlog.Domain.Entities.Systems
         public required string Name { get; set; }
         public required string Slug { get; set; }
         public string? Description { get; set; }
+        [DisplayName("Author")]
         public string? AuthorId { get; set; }
         [ForeignKey(nameof(AuthorId))]
         public virtual AppUser Author { get; set; }
         public string? Image { get; set; }
         public string? Content { get; set; }
         public string? Source { get; set; }
-
+        
         public string? EntryTag { get; set; }
         public int ViewCount { get; set; }
         public PostStatus Status { get; set; }
+        public string? Note { get; set; }
 
         public virtual ICollection<PostCategory> PostCategories { get; set; }
 
@@ -36,8 +39,8 @@ namespace VngBlog.Domain.Entities.Systems
     public enum PostStatus
     {
         WaitingForApproval = 1,
-        Rejected = 2,
-        Published = 3,
+        Published = 2,
+        Rejected = 3,
         Draft = 4
     }
 }
---
+++ VngBlog/VngBlog.Infrastructure/Migrations/20240217135452_Note_Post.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VngBlog.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Note_Post : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Note",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Categories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Note",
+                table: "Posts");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsActive",
+                table: "Categories",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+    }
+}
